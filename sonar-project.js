9eedc95fb8a78011b84dbb4b2b07389916b1a1b1
PREETI <prrana@deloitte.com>
2020-06-22 13:31:33 +0530

    sonar properties file added

--- sonar-project.js
+++
@@ -1,8 +0,0 @@
-const sonarqubeScanner = require('sonarqube-scanner');
-     sonarqubeScanner({
-       serverUrl: 'http://35.226.200.154:9000',
-       options : {
-       'sonar.sources': '.',
-       'sonar.inclusions' : 'src/**' // Entry point of your code
-       }
-     }, () => {});
